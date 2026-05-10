6b66773a7ad4f324d742c6b845bd577bd4e7e174
phessophissy <73696997+phessophissy@users.noreply.github.com>
2026-05-10 21:47:52 +0100

    feat(service): add contract name getter

--- src/MarketContractService.ts
+++ src/MarketContractService.ts
@@ -258,6 +258,10 @@ export class MarketContractService {
     return this.contractAddress;
   }
 
+  getContractName(): string {
+    return this.contractName;
+  }
+
   async getMarket(marketId: number): Promise<Market | null> {
     const response = await this.readContract('get-market', [
       { type: 'uint', value: marketId.toString() },
